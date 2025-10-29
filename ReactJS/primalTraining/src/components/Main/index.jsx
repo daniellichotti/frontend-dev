1e1f8a9e976b8a9f8f5a4ac2f9121eb0c8fe81fc
daniellichotti <danielclichotti@gmail.com>
2025-10-29 20:27:45 -0300

    sending codes

--- ReactJS/primalTraining/src/components/Main/index.jsx
+++ ReactJS/primalTraining/src/components/Main/index.jsx
@@ -1,6 +1,5 @@
-import { ContainerText, FirstContainer, FirstSection, SecondContainer, TextContainer } from "./styles";
-import FirstImage from "../../assets/FirstImage.png"
-import SecondImage from "../../assets/SecondImage.png"
+import { Container, TextContainer } from "./styles";
+
 
 export function Main() {
     return (
@@ -11,7 +10,38 @@ export function Main() {
                 </h1>
             </TextContainer>
 
-            
+            <Container>
+                <div id="firstImage" />
+                <div id="textCard">
+                    <h2>FOR THE COMMITTED</h2>
+                    <div>
+                        <p>Train like an athlete with top-tier equipment and expert programming. Whether you're building muscle or breaking PRs, we help you push past limits.</p>
+                        <p>About us</p>
+                    </div>
+                </div>
+                <div id="secondTextCard">
+                    <h2>FOR THE COMMITTED</h2>
+                    <div>
+                        <p>Train like an athlete with top-tier equipment and expert programming. Whether you're building muscle or breaking PRs, we help you push past limits.</p>
+                        <p>About us</p>
+                    </div>
+                </div>
+                <div id="thirdTextCard">
+                    <h2>FOR THE COMMITTED</h2>
+                    <div>
+                        <p>Train like an athlete with top-tier equipment and expert programming. Whether you're building muscle or breaking PRs, we help you push past limits.</p>
+                        <p>About us</p>
+                    </div>
+                </div>
+                <div id="secondImage" />
+            </Container>
+
+            <TextContainer>
+                <h1>
+                    Join The
+                    <strong>Community</strong>
+                </h1>
+            </TextContainer>
 
         </>
     )
--- ReactJS/primalTraining/src/components/Main/styles.jsx
+++ ReactJS/primalTraining/src/components/Main/styles.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import FirstImage from "../../assets/FirstImage.png"
+import SecondImage from "../../assets/SecondImage.png"
 
 export const TextContainer = styled.header`
     padding: 20px 2px;
@@ -18,4 +20,105 @@ export const TextContainer = styled.header`
         color: #808DFD;
     }
 `
+
+export const Container = styled.div`
+    background-color: #E9ECFF;
+
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    grid-template-rows: repeat(2, 480px);
+
+    #firstImage {
+        grid-column: 1 /span 2;
+        grid-row: 1;
+        background-image: url(${FirstImage}); 
+    }
+
+    #secondImage {
+        grid-column: 3;
+        grid-row: 2;
+        background-image: url(${SecondImage}); 
+    }
+
+    #textCard {
+        padding: 20px;
+        border: 1px solid black;
+        grid-column: 3;
+        grid-row: 1;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        h2 {
+            font-size: 45px;
+            font-weight: bold;
+        }
+
+        div {
+            font-size: 17px;
+
+            :last-child {
+                margin-top: 32px;
+                margin-bottom: 32px;
+                text-transform: uppercase;
+                
+            }
+        }
+    }
+
+    #secondTextCard {
+        padding: 20px;
+        border: 1px solid black;
+        grid-column: 1;
+        grid-row: 2;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        h2 {
+            font-size: 45px;
+            font-weight: bold;
+        }
+
+        div {
+            font-size: 17px;
+
+            :last-child {
+                margin-top: 32px;
+                margin-bottom: 32px;
+                text-transform: uppercase;
+                
+            }
+        }
+    }
+    #thirdTextCard {
+        padding: 20px;
+        border: 1px solid black;
+        grid-column: 2;
+        grid-row: 2;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        h2 {
+            font-size: 45px;
+            font-weight: bold;
+        }
+
+        div {
+            font-size: 17px;
+
+            :last-child {
+                margin-top: 32px;
+                margin-bottom: 32px;
+                text-transform: uppercase;
+                
+            }
+        }
+    }
+`
+
 
