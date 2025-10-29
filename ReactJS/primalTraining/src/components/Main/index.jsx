8157abd9e5ebe0feb38fd4a19b06a18eea7ab186
daniellichotti <danielclichotti@gmail.com>
2025-10-29 19:28:39 -0300

    sending codes

--- ReactJS/primalTraining/src/components/Main/index.jsx
+++ ReactJS/primalTraining/src/components/Main/index.jsx
@@ -1,4 +1,6 @@
-import { TextContainer } from "./styles";
+import { ContainerText, FirstContainer, FirstSection, SecondContainer, TextContainer } from "./styles";
+import FirstImage from "../../assets/FirstImage.png"
+import SecondImage from "../../assets/SecondImage.png"
 
 export function Main() {
     return (
@@ -8,6 +10,30 @@ export function Main() {
                     Train Hard. <strong>Live Better</strong>
                 </h1>
             </TextContainer>
+
+            <FirstSection>
+                <FirstContainer>
+                    <img src={FirstImage} alt="" />
+                    <ContainerText>
+                        <h2>for the commited</h2>
+                        <p>Train like an athlete with top-tier equipment and expert programming. Whether you're building muscle or breaking PRs, we help you push past limits.</p>
+                        <p>About us</p>
+                    </ContainerText>
+                </FirstContainer>
+                <SecondContainer>
+                    <ContainerText>
+                        <h2>Guided by experts</h2>
+                        <p>We believe in creating a positive environment where you can thrive. We're here to help you achieve your goals and unlock your full potential.
+                        </p>
+                    </ContainerText>
+                    <ContainerText>
+                        <h2>DYNAMIC OPEN GYM</h2>
+                        <p>Our facility is the optimal environment for strength training and performance, fully equipped with top-of-the-line tools, ample training areas, and a focus on functional movement.</p>
+                    </ContainerText>
+                    <img src={SecondImage} alt="" />
+                </SecondContainer>
+            </FirstSection>
+
         </>
     )
 };
--- ReactJS/primalTraining/src/components/Main/styles.jsx
+++ ReactJS/primalTraining/src/components/Main/styles.jsx
@@ -18,3 +18,43 @@ export const TextContainer = styled.header`
         color: #808DFD;
     }
 `
+
+export const FirstSection = styled.div`
+    background-color: #E9ECFF;
+`
+
+export const FirstContainer = styled.div`
+    display: flex;
+`
+export const SecondContainer = styled.div`
+    display: flex;
+`
+
+export const ContainerText = styled.div`
+    padding: 20px;
+    width: 426.67px;
+    height: 479px;
+
+    border: 1px solid black;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+        font-size: 45px;
+        font-weight: bold;
+        text-transform: uppercase;
+
+        margin-bottom: 163px;
+    }
+
+    p {
+        font-size: 17px;
+        font-weight: normal;
+    }
+
+    :last-child {
+        margin-top: 20px;
+    }
+`
