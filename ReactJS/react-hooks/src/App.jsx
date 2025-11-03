6e44e4202147446b8569ea727ab6cd16057f8148
daniellichotti <danielclichotti@gmail.com>
2025-11-03 20:25:18 -0300

    sending react codes

---
+++ ReactJS/react-hooks/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <>
+      <Button green={true}>Ok</Button>
+      <Button green={false}>Cancel</Button>
+    </>
+  )
+}
