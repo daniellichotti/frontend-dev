204714b21d6dbc728d6757445443324745d60338
daniellichotti <danielclichotti@gmail.com>
2025-11-05 18:17:44 -0300

    sendind codes

--- ReactJS/react-hooks/src/App.jsx
+++ ReactJS/react-hooks/src/App.jsx
@@ -1,10 +1,14 @@
 import { Button } from "./components/Button";
+import { Form } from "./components/Form";
 
 export function App() {
   return (
     <>
-      <Button green={true}>Ok</Button>
-      <Button green={false}>Cancel</Button>
+      {
+        //<Button green={true}>Ok</Button>
+        //<Button green={false}>Cancel</Button>
+      }
+      <Form />
     </>
   )
 }
